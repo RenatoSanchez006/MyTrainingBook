4443e4a2521f6630bd02f139c6abf695df88ce2b
Renato Sánchez <renatosancheznevarez@gmail.com>
2020-10-13 21:52:22 -0500

    Initial Commit

---
+++ MyTrainingBook/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyTrainingBook
+//
+//  Created by Renato Sanchez on 13/10/20.
+//  Copyright © 2020 Renato Sánchez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
